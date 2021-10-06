efbc5c8f0f62639d9ef9f0e4e2bcbefb5b5f2601
afesiuk <andriy.fesiuk@engineering.ait.com>
2021-10-06 13:07:58 +0300

    changed index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Welcome');
+    res.send('Welcome from branch test');
 })
 
 module.exports = router;
