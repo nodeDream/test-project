61be636b53751871ea2b4852c5b8112d7a8286ff
afesiuk <andriy.fesiuk@engineering.ait.com>
2021-10-08 15:07:21 +0300

    changed view

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Welcome from master branch');
+    res.send('Welcome from master branch clone');
 })
 
 module.exports = router;
